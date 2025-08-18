e57d47ec1b8c5027571dd3a660042cfb81d1d8e8
n.abbasi <nimaa9217@gmail.com>
2025-08-18 22:22:31 +0330

    fix axios header

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -10,8 +10,11 @@ const api = axios.create({
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
+    'Access-Control-Allow-Credentials': 'true',
+
   },
   withCredentials: true, // Enable cookies
+
 })
 
 // Request interceptor
