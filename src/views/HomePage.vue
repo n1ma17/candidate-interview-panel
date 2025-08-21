890315dc88d8901126c838a118e19f9e18c979f3
n.abbasi <nimaa9217@gmail.com>
2025-08-22 01:15:45 +0330

    fix color

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -10,7 +10,7 @@
     <div class="fixed bottom-0 left-0 right-0 p-4 md:mt-[38px] bg-white rounded-t-[12px] dark:bg-gray-900 border-t-3 dark:border-t-1 border-gray-100 dark:border-gray-700 md:relative md:p-0 md:bg-transparent md:border-0">
       <button
         @click="router.push('/interview')"
-        class="w-full md:w-auto inline-flex items-center justify-center rounded-lg bg-primary px-6 py-3 text-base font-medium text-white shadow-lg transition-all duration-200 hover:bg-primary-hover dark:hover:bg-grey-hover hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 dark:focus:ring-offset-gray-800"
+        class="w-full md:w-auto inline-flex items-center justify-center rounded-lg bg-primary px-6 py-3 text-base font-medium text-white shadow-lg transition-all duration-200 hover:bg-primary-hover dark:hover:bg-primary-hover hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 dark:focus:ring-offset-gray-800"
       >
         {{ t('home.startProcess') }}
       </button>
